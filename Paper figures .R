60e62283d8a0b384bee2f8398b73146ffc235531
Matthew Savoca <matthewsavoca@Matthews-MacBook-Pro.local>
2019-10-05 11:21:31 -0700

    changes for Hayden

--- Paper figures .R
+++ Paper figures .R
@@ -1,7 +1,11 @@
+
+
 # Load packages and data----
+
 library(ggplot2)
 library(ggpubr)
 library(tidyverse)
+
 # Allometric equations from Shirel's paper
 # creating fucntions from Shirel's paper for MW (in kg) for engulfment capacity in liters for each species where we have a known length
 Mass_SKR <- tribble(
@@ -13,19 +17,24 @@ Mass_SKR <- tribble(
 )
 
 #Brining in data for morphometrics and all flukebeat data 
-morphometrics <- read_csv("10:2 Data Sheet For Hayden.csv") 
+morphometrics <- read_csv("10:2 Data Sheet For Hayden.csv") %>% 
+  rename(Individual = `ID #`,
+         ID = "Whale") 
+
 #All Data
 d_all_swimming <- read_csv("10:2 Droned Tailbeats Info Hayden.csv") %>% 
+  left_join(select(morphometrics, Individual, ID), by = "Individual") %>% 
   select(-(X15:X19))
 
 #Separating max flukebeats from all data
 d_max_swimming <- read_csv("10:2 AllWhaleMaxEffortBeats.csv") %>% 
-  semi_join(d_all_swimming, by = colnames(d_all_swimming)[c(1:4, 7:8)]) %>% 
+  left_join(select(morphometrics, Individual, ID), by = "Individual") %>% 
+  semi_join(d_all_swimming, by = colnames(d_all_swimming)[c(1:4, 7:8, 15)]) %>% 
   mutate(effort_type = "Max")
 
 #Separating normal fluekbeats from all data
 d_reg_swimming <- d_all_swimming %>%  
-  anti_join(d_max_swimming, by = colnames(d_all_swimming)[c(1:4, 7:8)]) %>% 
+  anti_join(d_max_swimming, by = colnames(d_all_swimming)[c(1:4, 7:8, 15)]) %>% 
   mutate(effort_type = "Normal") 
 
 #Combining normal and max data, this is the data file that we will use 
@@ -34,6 +43,6 @@ d_combine_swimming <- bind_rows(d_reg_swimming, d_max_swimming) %>%
   mutate(Mass = (Length^slope)*10^intercept, 
          TPM = `Thrust Power`/Mass,
          Species = factor(Species, levels = c("Minke", "Humpback", "Fin", "Blue"))) %>% 
-  left_join(morphometrics, by = c("Species", "Individual")) 
+  left_join(morphometrics, by = c("Species", "Individual", "ID")) 
 
 
